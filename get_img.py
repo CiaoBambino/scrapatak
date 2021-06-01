0212c69940caeb275171c63b1c76a4e8b492c888
CiaoBambino <matteolani@live.fr>
2021-06-01 17:15:01 +0200

    supression des guillemets à 'global paths'

--- get_img.py
+++ get_img.py
@@ -3,5 +3,5 @@ import requests
 def get_img(url):
     reponse = requests.get(get_iurl(url))
     if reponse.status_code == 200:
-        with open('global paths', 'wb') as f:
+        with open(global paths, 'wb') as f:
         f.write(reponse.content)
