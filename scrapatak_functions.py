817deaca22f6815409ed6f264d8ccde4c8212bcd
CiaoBambino <matteolani@live.fr>
2021-06-10 20:15:48 +0200

    Refonte total du programme dans ces deux fonctions à partir du code déjà existant, désormais le main n'appelle que 4 fonctions qui font tout le travail de la même manière qu'avant
    passage a la branche scrapatakv2 sur git

--- scrapatak_functions.py
+++ scrapatak_functions.py
@@ -10,54 +10,166 @@ THIS CONTAIN ALL THE FUNCTIONS NECESSARY TO SCRAP PAGES
 """
 
 
+def get_all_category(cible):
+
+    result = requests.get(cible)
+
+    if result.status_code == 200:
+
+        soup = BeautifulSoup(result.text, features="html.parser")
+        category_link = []
+        category_name = []
+
+        for ultag in soup.findAll('ul', {'class': 'nav nav-list'}):
+
+            for litag in ultag.findAll('li'):
+
+                a = litag.find('a')
+                link = a['href']
+                category_link.append(link)
+                category_name.append(a.get_text())
+
+        return category_link, category_name
+
+
+def get_books_url_from_category(category_link, category_name):
+
+    i = 0
+    j = len(category_link)
+    book_links_per_category = [[] for x in range(j)]    # liste de liste contenant les liens de chaques catégories
+
+    for link in category_link:    # pour chaque lien(catégorie)
+
+        current_category = requests.get(link)
+
+        if current_category.status_code == 200:
+
+            soup = BeautifulSoup(current_category.text)
+
+            for li in soup.findALl('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'}):    # récupère url des livres
+
+                b = li.find('a')
+                url = b['href']
+                book_links_per_category[i].append(url)
+                next_page_link = None
+
+            if next_button(link) is True:  # s'il y a un bouton next donc une seconde page
+
+                _, next_page_link = next_button(link)  # on peut faire comme ça aussi "function()[1]"
+
+                next_page = requests.get(next_page_link)
+
+                if next_page.status_code == 200:
+
+                    soup2 = BeautifulSoup(next_page.text)
+
+                    for li in soup2.findALl('li', {
+                                            'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'}):  # on réitère l'opération
+
+                        a = li.find('a')
+                        urls = a['href']
+                        book_links_per_category[i].append(urls)
+
+                    while next_button(next_page_link) is True:  # tant qu'il y a un bouton next
+
+                        _, next_page_link = next_button(next_page_link)
+                        another_next_page = requests.get(next_page_link)
+
+                        if another_next_page.status_code == 200:
+
+                            soup3 = BeautifulSoup(another_next_page.text)
+
+                            for li in soup3.findALl('li', {
+                                                    'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'}):
+
+                                a = li.find('a')
+                                urlss = a['href']
+                                book_links_per_category[i].append(urlss)
+
+            i += 1
+
+        return book_links_per_category, category_name
+
+
+def scrap_target_page(book_links_per_category, category_name):
+    """
+    This function scrap the targeted page and return a CSV file and sleep for 3 between 7s each time end
+    and then write all in a CSV file
+    """
+
+    for i in range(len(book_links_per_category)):
+
+        for j in range(len(book_links_per_category[i])):
+
+            name = category_name[i]
+            url = book_links_per_category[i][j]
+            product_page_url = url
+            universal_product_code = get_universal_product_code(url)
+            title = get_title(url)
+            price_including_taxe = get_price_including_taxe(url)
+            price_excluding_taxe = get_price_excluding_taxe(url)
+            number_available = get_number_available(url)
+            product_description = get_product_description(url)
+            category = get_category(url)
+            review_rating = get_review_rating(url)
+            image_url = get_image_url(url)
+            get_img(image_url, category_name, i)
+            filename = "%s.csv" % name
+            save_path = '%s' % name
+
+            complete_name = os.path.join(save_path, filename)
+
+            with open(complete_name, 'a', newline="") as f:     # en mode ajout 'a' pour écrire à la suite
+                f.write(product_page_url + universal_product_code + title + price_including_taxe +
+                        price_excluding_taxe + number_available + product_description + category + review_rating +
+                        image_url)
+
+            sleep(randint(3, 7))
+
+
+def create_folder(_, category_name):
+
+    for i in category_name:
+
+        name = category_name[i]
+        directory_name = "%s" % name
+
+        os.mkdir(directory_name)
+        filename = "%s.csv" % name
+
+        with open(filename, 'w', newline="") as f:
+            f.write('product_page_url, universal_product_code, title, price_including_taxe,price_excluding_taxe,\
+                    number_available, product_description, category, review_rating, image_url')
+
+
 def next_button(link):
     """
     This function is doing 2 things :
     - First look for "Next" button that mean we have others pages to scrap and return true if its the case
     - Second get the link of the next page and return it
     """
-    soupsoja = BeautifulSoup(link.text)  # pas besoin de tester le lien déjà fait dans le main
-    child_tag = soupsoja.find('li', {'class': 'next'})
 
-    if child_tag:
+    next_button_link = requests.get(link)
 
-        a = child_tag.find('a')
-        next_page = a['href']
-        result = requests.get(next_page)
+    if next_button_link.status_code == 200:
 
-        if result.status_code == 200:
+        soup = BeautifulSoup(link.text)
 
-            return True, next_page
+        child_tag = soup.find('li', {'class': 'next'})
 
-    else:
+        if child_tag:
 
-        return False
+            a = child_tag.find('a')
+            next_page = a['href']
+            result = requests.get(next_page)
 
+            if result.status_code == 200:
 
-def scrap_target_page(url, filename, img_directory_name):
-    """
-    This function scrap the targeted page and return a CSV file and sleep for 3 between 7s each time end
-    and then write all in a CSV file
-    """
-    product_page_url = url
-    universal_product_code = get_universal_product_code(url)
-    title = get_title(url)
-    price_including_taxe = get_price_including_taxe(url)
-    price_excluding_taxe = get_price_excluding_taxe(url)
-    number_available = get_number_available(url)
-    product_description = get_product_description(url)
-    category = get_category(url)
-    review_rating = get_review_rating(url)
-    image_url = get_image_url(url)
-    get_img(image_url, img_directory_name)
+                return True, next_page
 
-    with open(filename, 'a', newline="") as f:     # en mode ajout 'a' pour écrire à la suite
+        else:
 
-        f.write(product_page_url + universal_product_code + title + price_including_taxe +
-                price_excluding_taxe + number_available + product_description + category + review_rating +
-                image_url)
-
-    sleep(randint(3, 7))
+            return False
 
 
 def get_universal_product_code(url):
@@ -65,7 +177,7 @@ def get_universal_product_code(url):
     This function take the UPC and put it in a list in first place
     """
     result = requests.get(url)
-    universal_product_code = None
+    universal_product_code = "Unknow"
 
     if result.status_code == 200:  # result.ok
         print(result)
@@ -81,7 +193,7 @@ def get_universal_product_code(url):
             j += 1  # incrémente le compteur du tableau pour passer à la clef-valeur suivante
 
         universal_product_code = argument[0]
-    return universal_product_code
+        return universal_product_code
 
 
 def get_title(url):
@@ -103,7 +215,7 @@ def get_price_including_taxe(url):
     This function take the price including taxe and put it in a list in fourth place
     """
     result = requests.get(url)
-    price_including_tax = None
+    price_including_tax = "Unknow"
 
     if result.status_code == 200:  # result.ok
         print(result)
@@ -119,7 +231,7 @@ def get_price_including_taxe(url):
             j += 1
 
         price_including_tax = argument[3]
-    return price_including_tax
+        return price_including_tax
 
 
 def get_price_excluding_taxe(url):
@@ -127,7 +239,7 @@ def get_price_excluding_taxe(url):
     This function take the price excluding taxe and put it in a list in third place
     """
     result = requests.get(url)
-    price_excluding_tax = None
+    price_excluding_tax = "Unknow"
 
     if result.status_code == 200:  # result.ok
         print(result)
@@ -143,7 +255,7 @@ def get_price_excluding_taxe(url):
             j += 1
 
         price_excluding_tax = argument[2]
-    return price_excluding_tax
+        return price_excluding_tax
 
 
 def get_number_available(url):
@@ -151,7 +263,7 @@ def get_number_available(url):
     This function take the number available of books and put it in a list in sixs place
     """
     result = requests.get(url)
-    number_available = None
+    number_available = "Unknow"
 
     if result.status_code == 200:  # result.ok
         print(result)
@@ -167,7 +279,7 @@ def get_number_available(url):
             j += 1
 
         number_available = argument[5]
-    return number_available
+        return number_available
 
 
 def get_product_description(url):
@@ -183,7 +295,7 @@ def get_product_description(url):
         soup = BeautifulSoup(result.text)
         p = soup.find('p').get_text()     # peut etre un .string
         product_description = p
-    return product_description
+        return product_description
 
 
 def get_category(url):
@@ -212,7 +324,7 @@ def get_review_rating(url):
     This function take the review rating number and put it in a list in seventh place and return it
     """
     result = requests.get(url)
-    review_rating = None
+    review_rating = "Unknow"
 
     if result.status_code == 200:  # result.ok
         print(result)
@@ -228,7 +340,7 @@ def get_review_rating(url):
             j += 1
 
         review_rating = argument[6]
-    return review_rating
+        return review_rating
 
 
 def get_image_url(url):
@@ -249,18 +361,22 @@ def get_image_url(url):
             print(img)
             link = img['src']
             image_url = link
-    return image_url
+        return image_url
 
 
-def get_img(url, img_directory_name):
+def get_img(url, category_name, i):
     """
     This function download image with the title in name in the folders of the programme
     """
+    name = category_name[i]
     image_name = "%s.jpg" % get_title(url)
     link = get_image_url(url)
     response = requests.get(link)
+    save_path = '%s' % name
+
+    complete_name = os.path.join(save_path, image_name)
 
     if response.status_code == 200:
 
-        with open(os.path.join(img_directory_name, image_name), 'wb') as f:
+        with open(complete_name, 'wb') as f:
             f.write(response.content)
--- scrapatakv2.py
+++ scrapatakv2.py
@@ -1,7 +1,1 @@
-from functions import create_folder, get_all_category, get_books_url_from_category, scrap_target_page
-
-cible = 'http://books.toscrape.com/index.html'
-
-create_folder(get_all_category(cible))
-scrap_target_page(get_books_url_from_category(get_all_category(cible)))
 
